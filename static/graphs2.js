325c1e2831480a6b408c144798fb0003569f5fc4
VighneshS <vighneshsivaraman@gmail.com>
2021-06-28 16:42:25 -0500

    Quiz 4 Question 6 complete

--- static/graphs2.js
+++ static/graphs2.js
@@ -12,16 +12,17 @@ $(function () {
 function loadChart() {
     spinner.show()
     figure.hide()
-    var payLoad = {
-        'numberOfItems': $('#numberOfRecent').val()
-    }
+    var countryName = $("#countryName")
     $.ajax({
         type: 'POST',
         url: "/graphs/2",
         contentType: "application/json",
         dataType: 'json',
-        data: JSON.stringify(payLoad)
+        data: JSON.stringify({
+            'countryName': countryName.val()
+        })
     }).done(function (data) {
+        // $(this).addClass("done");
         createChart(data)
         spinner.hide()
         figure.show()
@@ -29,71 +30,119 @@ function loadChart() {
 }
 
 function createChart(data) {
+    var categories = _.map(data, 'magRange');
+    var values = _.map(data, 'value');
     Highcharts.chart('bar-chart', {
         chart: {
             height: "50%",
-            type: 'scatter',
-            zoomType: 'xy'
+            type: 'column'
         },
         title: {
-            text: 'Magnitude vs Depth of Recent Earthquake data'
+            text: 'Number of Volcanoes based on Longitude range'
         },
         subtitle: {
-            text: 'Scatter plot'
+            text: 'Bar Chart'
         },
         xAxis: {
+            categories: categories,
+            crosshair: true,
             title: {
-                enabled: true,
-                text: 'Magnitude'
-            },
-            startOnTick: true,
-            endOnTick: true,
-            showLastLabel: true
-        },
-        yAxis: {
-            title: {
-                text: 'Depth'
+                text: 'Volcano Longitude Range'
             }
         },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 100,
-            y: 70,
-            floating: true,
-            backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
-            borderWidth: 1
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of Volcanoes'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
         },
         plotOptions: {
-            scatter: {
-                marker: {
-                    radius: 5,
-                    states: {
-                        hover: {
-                            enabled: true,
-                            lineColor: 'rgb(100,100,100)'
-                        }
-                    }
+            series: {
+                color: '#EE6363'
+            },
+            column: {
+                dataLabels: {
+                    enabled: true
                 },
-                states: {
-                    hover: {
-                        marker: {
-                            enabled: false
-                        }
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<b>{series.name}</b><br>',
-                    pointFormat: '{point.x}, {point.y} kg'
+                zones: [{
+                    value: _.max(_.map(data, 'value')), // Values up to max value (not including) ...
+                    color: '#79973F' // ... have the color pea.
+                }, {
+                    color: '#EE6363' // Values from max value (including) and up have the color fire brick red
+                }],
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Number of Volcanoes',
+            data: values
+
+        }]
+    });
+
+    var pieChartData = []
+    data.map(item => {
+        pieChartData.push(
+            _.mapKeys(item, (value, key) => {
+                let newKey = key;
+                if (key === 'magRange') {
+                    newKey = 'name';
+                }
+
+                if (key === 'value') {
+                    newKey = 'y';
+                }
+
+                return newKey;
+            })
+        )
+    });
+
+    Highcharts.chart('pie-chart', {
+        chart: {
+            height: "50%",
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Number of Volcanoes based on Longitude range'
+        },
+        subtitle: {
+            text: 'Pie Chart'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    connectorColor: 'silver'
                 }
             }
         },
         series: [{
-            name: 'Magnitude vs Depth',
-            color: 'rgba(223, 83, 83, .5)',
-            data: data
-
+            name: 'Percentage of Volcanoes',
+            data: pieChartData
         }]
     });
 }
